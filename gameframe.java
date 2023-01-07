c3cdd053c5d85ccb3c493c8857197025457ac86b
Vinod Kumar Reddy Gorantla <104248330+Vk6465@users.noreply.github.com>
2023-01-07 19:48:08 +0530

    Add files via upload

---
+++ gameframe.java
@@ -0,0 +1,19 @@
+package snakegame;
+
+import javax.swing.JFrame;
+
+public class gameframe extends JFrame{
+	gameframe(){
+		
+		
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+		
+	}
+}
---
+++ snakegames1.java
@@ -0,0 +1,10 @@
+package snakegame;
+
+public class snakegames1 {
+
+	public static void main(String[] args) {
+		new gameframe();
+
+	}
+
+}
